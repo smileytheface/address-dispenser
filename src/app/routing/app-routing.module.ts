443c56253ba63587be3e82ab166a6518a7bfbe12
Carl Mastny <mr.smileytheface@gmail.com>
2021-05-12 11:25:37 -0500

    Guard routes when not authenticated

--- src/app/routing/app-routing.module.ts
+++ src/app/routing/app-routing.module.ts
@@ -11,47 +11,75 @@ import { AddManyComponent } from '../addresses/add-addresses/add-many/add-many.c
 import { AddWriterComponent } from '../writers/add-writer/add-writer.component';
 import { CustomSendComponent } from '../send-addresses/custom-send/custom-send.component';
 import { SignInComponent } from '../auth/sign-in/sign-in.component';
+import { AuthGuard } from '../auth/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'send-addresses', component: SendAddressesComponent },
-  { path: 'send-addresses/custom-send', component: CustomSendComponent },
+  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  {
+    path: 'send-addresses',
+    component: SendAddressesComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'send-addresses/custom-send',
+    component: CustomSendComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'available-addresses',
     component: AvailableAddressesComponent,
+    canActivate: [AuthGuard],
   },
   {
     path: 'available-addresses/add-addresses',
     component: AddAddressesComponent,
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
         pathMatch: 'full',
         redirectTo: 'add-one',
+        canActivate: [AuthGuard],
       },
       {
         path: 'add-one',
         component: AddOneComponent,
+        canActivate: [AuthGuard],
       },
       {
         path: 'add-many',
         component: AddManyComponent,
+        canActivate: [AuthGuard],
       },
     ],
   },
   {
     path: 'available-addresses/edit/:addressId',
     component: AddOneComponent,
+    canActivate: [AuthGuard],
   },
-  { path: 'assigned-addresses', component: AssignedAddressesComponent },
-  { path: 'writers', component: WritersComponent },
-  { path: 'writers/add-writer', component: AddWriterComponent },
-  { path: 'writers/edit/:writerId', component: AddWriterComponent },
+  {
+    path: 'assigned-addresses',
+    component: AssignedAddressesComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'writers', component: WritersComponent, canActivate: [AuthGuard] },
+  {
+    path: 'writers/add-writer',
+    component: AddWriterComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'writers/edit/:writerId',
+    component: AddWriterComponent,
+    canActivate: [AuthGuard],
+  },
   { path: 'sign-in', component: SignInComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGuard],
 })
 export class AppRoutingModule {}
