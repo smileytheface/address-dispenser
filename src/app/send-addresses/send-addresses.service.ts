92abce613d09bc410d6589e4b89741106618585e
Carl Mastny <mr.smileytheface@gmail.com>
2021-05-15 16:46:14 -0500

    Update send addresses-service to use correct api url

--- src/app/send-addresses/send-addresses.service.ts
+++ src/app/send-addresses/send-addresses.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Address } from '../shared/models/address.model';
+import { Subject } from 'rxjs';
+
 import { TextData } from '../shared/models/text-data.model';
 import { EmailData } from '../shared/models/email-data.model';
-import { Subject } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+const BACKEND_URL = environment.apiUrl + '/addresses';
 
 @Injectable({
   providedIn: 'root',
@@ -17,52 +20,46 @@ export class SendAddressesService {
   constructor(private http: HttpClient) {}
 
   textAddresses(textData: TextData) {
-    this.http
-      .put('http://localhost:3000/api/addresses/text-addresses', textData)
-      .subscribe(
-        (confirmation) => {
-          console.log(confirmation);
-          this.messageSent.next('Message sent successfully');
-        },
-        // Handling possible errors when texting addresses
-        (error) => {
-          console.error(error);
-          // Error for invalid phone number
-          if (error.error.error.code === 21211) {
-            this.messageNotSent.next('Invalid phone number, message not sent');
-          }
-          // Error for database issues
-          else if (error.source === 'database') {
-            this.messageNotSent.next(
-              'Internal error, could not assign address'
-            );
-          }
-          // Other errors
-          else {
-            this.messageNotSent.next(
-              'Error occured, addresses may not have been sent'
-            );
-          }
+    this.http.put(BACKEND_URL + '/text-addresses', textData).subscribe(
+      (confirmation) => {
+        console.log(confirmation);
+        this.messageSent.next('Message sent successfully');
+      },
+      // Handling possible errors when texting addresses
+      (error) => {
+        console.error(error);
+        // Error for invalid phone number
+        if (error.error.error.code === 21211) {
+          this.messageNotSent.next('Invalid phone number, message not sent');
         }
-      );
-  }
-
-  emailAddresses(emailData: EmailData) {
-    console.log(emailData);
-    this.http
-      .put('http://localhost:3000/api/addresses/email-addresses', emailData)
-      .subscribe(
-        (confirmation) => {
-          console.log(confirmation);
-          this.messageSent.next('Message sent successfully');
-        },
-        (error) => {
-          console.error(error);
+        // Error for database issues
+        else if (error.source === 'database') {
+          this.messageNotSent.next('Internal error, could not assign address');
+        }
+        // Other errors
+        else {
           this.messageNotSent.next(
             'Error occured, addresses may not have been sent'
           );
         }
-      );
+      }
+    );
+  }
+
+  emailAddresses(emailData: EmailData) {
+    console.log(emailData);
+    this.http.put(BACKEND_URL + '/email-addresses', emailData).subscribe(
+      (confirmation) => {
+        console.log(confirmation);
+        this.messageSent.next('Message sent successfully');
+      },
+      (error) => {
+        console.error(error);
+        this.messageNotSent.next(
+          'Error occured, addresses may not have been sent'
+        );
+      }
+    );
   }
 
   getMesageSentListener() {
